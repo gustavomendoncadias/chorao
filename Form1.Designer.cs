d9778be078fdf56140f2ef50277ba79643e0b285
gustavo.mdias5 <gustavo.mdias5@AME0613371W10-1>
2024-03-20 22:12:51 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace chorao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.lbl_cpf = new System.Windows.Forms.Label();
+            this.lbl_cidade = new System.Windows.Forms.Label();
+            this.lbl_endereco = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(12, 23);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(55, 20);
+            this.lbl_nome.TabIndex = 1;
+            this.lbl_nome.Text = "Nome:";
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(12, 70);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(54, 20);
+            this.lbl_idade.TabIndex = 2;
+            this.lbl_idade.Text = "Idade:";
+            // 
+            // lbl_cpf
+            // 
+            this.lbl_cpf.AutoSize = true;
+            this.lbl_cpf.Location = new System.Drawing.Point(12, 110);
+            this.lbl_cpf.Name = "lbl_cpf";
+            this.lbl_cpf.Size = new System.Drawing.Size(44, 20);
+            this.lbl_cpf.TabIndex = 3;
+            this.lbl_cpf.Text = "CPF:";
+            // 
+            // lbl_cidade
+            // 
+            this.lbl_cidade.AutoSize = true;
+            this.lbl_cidade.Location = new System.Drawing.Point(12, 148);
+            this.lbl_cidade.Name = "lbl_cidade";
+            this.lbl_cidade.Size = new System.Drawing.Size(63, 20);
+            this.lbl_cidade.TabIndex = 4;
+            this.lbl_cidade.Text = "Cidade:";
+            // 
+            // lbl_endereco
+            // 
+            this.lbl_endereco.AutoSize = true;
+            this.lbl_endereco.Location = new System.Drawing.Point(12, 187);
+            this.lbl_endereco.Name = "lbl_endereco";
+            this.lbl_endereco.Size = new System.Drawing.Size(82, 20);
+            this.lbl_endereco.TabIndex = 5;
+            this.lbl_endereco.Text = "Endereço:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(73, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(100, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(73, 110);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbl_endereco);
+            this.Controls.Add(this.lbl_cidade);
+            this.Controls.Add(this.lbl_cpf);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.lbl_nome);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.Label lbl_cpf;
+        private System.Windows.Forms.Label lbl_cidade;
+        private System.Windows.Forms.Label lbl_endereco;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
